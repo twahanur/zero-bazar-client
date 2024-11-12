1b36886935038860aa5afa9092e9b09bfb30576c
twahanur <thohanur143@gmail.com>
2024-11-12 23:44:06 +0600

    update server link

--- config.ts
+++ config.ts
@@ -6,7 +6,7 @@ const checkConfig = (server: string): Config | {} => {
   switch (server) {
     case "production":
       config = {
-        baseUrl: "http://localhost:8000",
+        baseUrl: "zero-bazar-server.vercel.app",
       };
       break;
     case "local":
